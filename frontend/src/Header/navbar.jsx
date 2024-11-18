318fea874b5ce58d7c4fa01319bc9a6e7b440286
SameerSingh <sameerkhobra474@gmail.com>
2024-11-19 00:57:02 +0530

    optimized code for navbar

--- frontend/src/Header/navbar.jsx
+++ frontend/src/Header/navbar.jsx
@@ -1,36 +1,34 @@
-import React, { useState } from "react";
+import React ,{useState} from 'react'
 
-export const Navbar = () => {
-  const [clickedIndex, setClickedIndex] = useState(null);
+export const Navbar=()=>{
+    const [clickedIndex,setClickedIndex]=useState(null)
 
-  const handleClick = (index) => {
-    setClickedIndex(index);
-    setTimeout(() => setClickedIndex(null), 300); // Reset after animation
-  };
+   const handleClick=(index)=>{
+        setClickedIndex(index)
+        setTimeout(()=>setClickedIndex(null),300) // it will be reseted after doing animation
+   }
+   {/* yha branch wise ya compney wise filter add kr skte hai */}
 
-  return (
-    <nav className="bg-purple-700 text-white">
-      <ul className="flex items-center justify-between mx-auto px-4 py-4 container">
-        {[
-          { name: "Home", href: "#home" },
-          { name: "Search", href: "#search" },
-          { name: "Statistics", href: "#statistics" },
-          { name: "Past Record", href: "#past-record" },
-          { name: "About", href: "#about" },
-        ].map((item, index) => (
-          <li
-            key={index}
-            onClick={() => handleClick(index)}
-            className={`${
-              clickedIndex === index ? "scale-110" : "scale-100"
-            } transition-transform duration-300 ease-in-out`}
-          >
-            <a href={item.href} className="hover:text-orange-300">
-              {item.name}
-            </a>
-          </li>
-        ))}
-      </ul>
-    </nav>
-  );
-};
+    return (
+        <nav className='bg-purple-700 text-white'>
+            <ul className='flex items-center justify-between mx-auto px-4 py-4 container' >
+                {[
+                    {name:"Home",href:"#home"},
+                    {name:"Search",href:"#Search"},
+                    {name:"Statistics",href:"#Statistics"},
+                    {name:"Past Record",href:"#past-record"},
+                    {name:"About",href:"#about"},
+                ].map((item,index)=>(
+                    <li
+                        key={index}
+                        onClick={()=>handleClick(index)}
+                        className={`${clickedIndex===index?"scale-110":"scale-100"} 
+                        transition-transform duration-300 ease-in-out`}
+                    >
+                        <a href={item.href} className='hover:text-orange-300'>{item.name}</a>
+                    </li>
+                ))}    
+            </ul>
+        </nav>
+    )
+}
