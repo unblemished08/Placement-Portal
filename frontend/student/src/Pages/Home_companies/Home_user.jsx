52256b3505b35f26bf908b475b70547a190df065
Harshit Anand <harshitanand893@gmail.com>
2025-01-17 22:17:00 +0530

    home page ideation

--- frontend/student/src/Pages/Home_companies/Home_user.jsx
+++ frontend/student/src/Pages/Home_companies/Home_user.jsx
@@ -6,7 +6,11 @@ const Home_user = () => {
   const { companies } = useContext(StoreContext);
 
   return (
-      <CompaniesSlider companies={companies} />
+    <>
+    <div className="border-solid border-2 border-sky-500 h-20">Notification bar</div>
+    <CompaniesSlider companies={companies} />
+    <div className="border-solid border-2 border-sky-500 h-20">Footer - tnp linkedin mail copyright reserved</div>
+    </>
   );
 };
 
