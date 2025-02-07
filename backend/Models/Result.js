026151886614f8e988025dc83f26c0dbb2400614
Harshit Anand <harshitanand893@gmail.com>
2025-02-07 15:10:41 +0530

    Result schema added

---
+++ backend/Models/Result.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const ResultSchema = new mongoose.Schema(
+    {
+        rollNo: {
+            type: String,
+            required: true,
+            ref:"Student",
+            unique: true,
+        },
+
+        name:{
+            type:String,
+            required: true,
+            ref:"Company",
+        },
+
+        job_id:{ 
+            type:String,
+            required:true,
+            ref:"Company",
+        },
+
+        ctc:{ //ctc/stipend
+            type:String,
+            required:true,
+        },
+
+        job_profile: { //job profile
+            type: [String],
+            enum: ["Intern + Full Time","Full Time", "Intern", "Research Intern/Project"],
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+export default mongoose.model("Result", ResultSchema);
