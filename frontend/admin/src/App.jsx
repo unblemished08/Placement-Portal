8e85921930c9f10dfe4214ac0bdd00743c133b82
Harshit Anand <harshitanand893@gmail.com>
2025-02-10 16:41:57 +0530

    add signup route and logo; update package-lock.json

--- frontend/admin/src/App.jsx
+++ frontend/admin/src/App.jsx
@@ -1,6 +1,7 @@
 import Form from "./form";
 import React from "react";
 import Login from "./login";
+import Signup from "./signup";
 import {Route,Routes} from "react-router-dom"
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Form/>}/>
       <Route path="/login" element={<Login/>}/>
+      <Route path="/signup" element={<Signup/>}/>
     </Routes>
     
     </>
